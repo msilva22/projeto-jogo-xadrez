a81da21a72c2f96f937af79eaced558474328f4f
Moisés Silva <moises.ambiental@hotmail.com>
2023-03-23 17:33:29 -0300

    First class: Position

--- src/applicaton/Program.java
+++ src/applicaton/Program.java
@@ -1,9 +1,12 @@
 package applicaton;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Position pos  = new Position(3,5);
+		System.out.println(pos);
 
 	}
 
